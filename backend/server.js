70d65397e461b6e605498369dc42ac321973c79c
mdmarufsarker <mdmarufsarker2021@gmail.com>
2022-07-21 16:05:35 +0600

    frontend and backend server setup done

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import 'dotenv/config'
 
 const app = express();
+console.log(process.env.PORT);
 
-const port = 7777;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 7777, () => {
+    console.log(`Server is running`);
+    console.log(`http://localhost:${process.env.PORT || 7777}`);
 })
